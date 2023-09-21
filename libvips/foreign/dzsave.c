d09c4509d28e6dc5c8c4088212494e03278ad515
DarthSim <darthsim@gmail.com>
2023-09-21 19:11:01 +0300

    Fix invalid UTF-8 strings instead of ignoring them

--- libvips/foreign/dzsave.c
+++ libvips/foreign/dzsave.c
@@ -1375,7 +1375,7 @@ static void
 build_scan_property( VipsDbuf *dbuf, VipsImage *image, 
 	const char *vips_name, const char *szi_name )
 {
-	const char *str;
+	char *str;
 	GValue value = { 0 };
 	GValue save_value = { 0 };
 	GType type;
@@ -1399,12 +1399,8 @@ build_scan_property( VipsDbuf *dbuf, VipsImage *image,
 	}
 	g_value_unset( &value );
 
-	if( !(str = vips_value_get_save_string( &save_value )) ) {
-		g_value_unset( &save_value );
-		return;
-	}
-
-	if( !g_utf8_validate( str, -1, NULL ) ) {
+	if( !( str = g_utf8_make_valid(
+		vips_value_get_save_string( &save_value ), -1 ) ) ) {
 		g_value_unset( &save_value );
 		return;
 	}
@@ -1418,6 +1414,8 @@ build_scan_property( VipsDbuf *dbuf, VipsImage *image,
 	vips_dbuf_write_amp( dbuf, str );
 	vips_dbuf_writef( dbuf, "</value>\n" );
 	vips_dbuf_writef( dbuf, "    </property>\n" );
+
+	g_free( str );
 
 	g_value_unset( &save_value );
 }
